91079c9f6ec83478e166fa5294bd8b5a73823829
Hanna Papova <seal.annuska@gmail.com>
2022-04-28 13:59:49 -0400

    feat: add sort by height solution

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let numbersArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      numbersArr.push(arr[i]);
+    }
+  }
+
+  if (numbersArr.length === 0) return arr; // if all numbers are -1
+
+  numbersArr = numbersArr.sort((a, b) => a - b);
+
+  if (numbersArr.length === arr.length) return numbersArr // if all numbers without -1 return sorted arr
+
+  let result = [];
+  let pointer = 0;
+  let deletedCount = 0;
+
+  while (pointer < arr.length) {
+    if (arr[pointer] === -1) {
+      if (deletedCount > 0) {
+        let chunk = numbersArr.splice(0, deletedCount);
+        result = [...result, ...chunk];
+        deletedCount = 0;
+      }
+        result.push(arr[pointer]);
+      } else {
+      deletedCount += 1;
+    }
+
+    if (pointer === arr.length - 1 && deletedCount > 0) {
+      let chunk = numbersArr.splice(0, deletedCount);
+      result = [...result, ...chunk];
+    }
+
+    pointer++;
+  }
+
+  return result;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
+
+// sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]);
+// sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180])
