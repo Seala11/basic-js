18bee30eb49c13f7d87aa7e1bbad35f10420dcdc
Hanna Papova <seal.annuska@gmail.com>
2022-04-29 12:29:24 -0400

    feat: add delete digit solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -9,13 +9,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * For n = 152, the output should be 52
+ * deleteDigit(222219), 22229
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let num = Math.abs(n); // in case negative num
+
+  if (num <= 9) return n; // only one digit;
+
+  if (num.toString().length === 2) { // if only 2 digits compare them;
+    let first = +num.toString().charAt(0);
+    let second = +num.toString().charAt(1);
+    return Math.max(first, second)
+  }  
+
+  // num has more than 2 digits
+  let max = -Infinity;
+  const length = n.toString().length; // get the length
+  let fromIndex = 0;
+  let toIndex = 1;
+
+  // cut one by one and compare
+  while (toIndex < length) {
+    let str = num.toString();
+    let curr = str.substring(0, fromIndex) + str.substring(toIndex);
+    console.log(curr);
+    max = Math.max(+curr, +max);
+    fromIndex++;
+    toIndex++;
+  }
+
+  return max;
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
+
+
+// deleteDigit(90)
