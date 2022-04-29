b20b2b107cbe82b79b23f3e0a20a52ef7fdc21b5
Hanna Papova <seal.annuska@gmail.com>
2022-04-29 16:31:28 -0400

    feat: add file name solution

--- src/file-names.js
+++ src/file-names.js
@@ -15,11 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let map = new Map();
+  let uniqueNames = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let curr = names[i];
+
+    // if such name already exist
+    if (map.has(curr)) {
+      let int = map.get(curr); // get the numbers of such files that we found
+
+      // find new not repated name
+      let unique = false;
+      while (!unique) {
+        // set new file name
+        let name = `${curr}(${int})`;
+        if (!map.has(name)) { // if it s unique save else keep looking
+          curr = name;
+          unique = true;
+        } else {
+          int++;
+        }
+      }
+
+      uniqueNames.push(curr);
+      // update map => add to number of times we found that file
+      map.set(curr, 1);
+    } else {
+      uniqueNames.push(curr);
+
+      // update map
+      map.set(curr, 1);
+    }
+  }
+
+  console.log(uniqueNames);
+  return uniqueNames;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
+
+renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc']);
+// ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']
