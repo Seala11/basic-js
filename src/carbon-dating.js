1a365b1394df9acebb09021f9efc7a5e6cb1c5a5
Hanna Papova <seal.annuska@gmail.com>
2022-04-29 11:57:07 -0400

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,36 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let sampleNumber = +sampleActivity;
+  let result;
+  if (isNaN(+sampleNumber) || sampleNumber <= 0 || sampleNumber > 15 || typeof sampleActivity !== 'string' || !isFinite(sampleNumber)) {
+    console.log('false');
+    return false;
+  }
+
+  // the rate constant for the reaction from its half-life (5730 yr) 
+  let k = 0.693 / HALF_LIFE_PERIOD;
+
+  result = Math.ceil(Math.log((MODERN_ACTIVITY / sampleNumber)) / k)
+
+  return result;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
+
+// dateSample(3);
+// dateSample(' \n\t\r')
