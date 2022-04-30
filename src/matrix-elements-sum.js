091d66fa9b8f1673514a6d91b3f73fb279642d0c
Hanna Papova <seal.annuska@gmail.com>
2022-04-29 20:06:29 -0400

    feat: add matrix element count solution

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  if (!matrix) return 0;
+
+  const rows = matrix.length;
+  const columns = matrix[0].length
+
+  let counter = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let curr = matrix[i][j]
+  
+      if (i === 0) { // first row cant be below 0; just count numbers
+        counter += curr;
+      } else { // check if not below 0
+        if (!belowZero(matrix, i, j)) counter += curr;
+      }
+    }
+  }
+
+  return counter;
+}
+
+const belowZero = (matrix, i, j) => {
+  let checkIfZero = matrix[i - 1][j]
+  return checkIfZero === 0;
 }
 
 module.exports = {
